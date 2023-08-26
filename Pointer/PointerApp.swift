02f325a03904d60eeeedb2dd412850bffd9b6c60
Brandon Johns <56181912+brandonjohns914@users.noreply.github.com>
2023-08-26 16:43:30 -0700

    Initial Commit

---
+++ Pointer/PointerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PointerApp.swift
+//  Pointer
+//
+//  Created by Brandon Johns on 8/26/23.
+//
+
+import SwiftUI
+
+@main
+struct PointerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
